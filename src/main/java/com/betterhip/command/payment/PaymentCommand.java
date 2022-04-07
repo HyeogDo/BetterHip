feca2b736d8f711b4c0b64ebdcbf8a06bfaf339a
jeongchanjin <jungcj1119@gmail.com>
2022-04-07 18:51:01 +0900

    220407_정찬진 paymentCommand, PaymentListCommand, PaymentSuccessCommand 작성

--- src/main/java/com/betterhip/command/payment/PaymentCommand.java
+++ src/main/java/com/betterhip/command/payment/PaymentCommand.java
@@ -1,16 +1,26 @@
 package com.betterhip.command.payment;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.betterhip.command.BetterhipCommand;
+import com.betterhip.dao.payment.PaymentDao;
+import com.betterhip.dto.payment.PaymentDto;
 
 public class PaymentCommand implements BetterhipCommand {
 
 	@Override
 	public void excute(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
-
+		int purchase_id = Integer.parseInt(request.getParameter("purchase_id"));
+		
+		PaymentDao dao = new PaymentDao();
+		ArrayList<PaymentDto> dtos = dao.payment(purchase_id);
+		request.setAttribute("payment", dtos);
+		
+		
+		
 	}
 
 }
